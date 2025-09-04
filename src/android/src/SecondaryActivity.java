ef40515edb4b6fa2188f350891a1db5366401d84
Jasphine <jasphinekhoolf@hotmail.com>
2025-09-04 15:28:40 +0800

    Update SecondaryActivity.java

--- src/android/src/SecondaryActivity.java
+++ src/android/src/SecondaryActivity.java
@@ -16,7 +16,11 @@ import android.widget.TextView;
 // |  Google Imports
 // ----------------------------------------------------------------------------
 import com.google.android.gms.common.api.CommonStatusCodes;
-import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.common.InputImage;
 
 public class SecondaryActivity extends Activity implements View.OnClickListener {
   // ----------------------------------------------------------------------------
